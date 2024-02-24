92fa4016b925d6b9cd16db1d001d7a66a45d29f2
Kartik <kartik20044@gmail.com>
2024-02-24 02:22:44 -0300

    menu featured is done

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,8 +1,23 @@
-import { featuredProducts } from "@/data";
+import { ProductType } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const Featured = () => {
+const getData = async ()=>{
+  const res = await fetch("http://localhost:3000/api/products",{
+    cache:"no-store"
+  })
+
+  if(!res.ok){
+    throw new Error("Failed!");
+  }
+
+  return res.json()
+}
+
+const Featured = async () => {
+
+  const featuredProducts:ProductType[] = await getData()
+
   return (
     <div className="w-screen overflow-x-scroll text-red-500">
       {/* WRAPPER */}
@@ -11,12 +26,12 @@ const Featured = () => {
         {featuredProducts.map((item) => (
           <div
             key={item.id}
-            className="w-screen h-[70vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
           >
             {/* IMAGE CONTAINER */}
             {item.img && (
               <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
-                <Image src={item.img} alt="" fill className="object-contain " />
+                <Image src={item.img} alt="" fill className="object-contain" />
               </div>
             )}
             {/* TEXT CONTAINER */}
