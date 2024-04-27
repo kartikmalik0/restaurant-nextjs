27c5529ec979152cc4b93c65d7dd5184539eac19
Kartik <kartik20044@gmail.com>
2024-04-27 07:44:46 -0300

    add product button updated and cart icon also

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useCartStore } from "@/utils/store";
+import { Button } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,26 +15,24 @@ const CartIcon = () => {
     useCartStore.persist.rehydrate();
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     useCartStore.persist.rehydrate()
-  },[])
+  }, [])
   return (
     <Link href={session?.user.isAdmin ? "/add" : "/cart"}>
       <div className="flex items-center gap-4">
-        <div className="relative w-8 h-8 md:w-5 md:h-5">
-          <Image
-            src="/cart.png"
-            alt=""
-            fill
-            sizes="100%"
-            className="object-contain"
-          />
-        </div>
-        {session?.user.isAdmin ? (
-          <button className="p-1 bg-red-500 text-white rounded-md">Add product</button>
-        ) : (
-          <span>Cart ({totalItems})</span>
-        )}
+        {
+          !session?.user.isAdmin ? <div className="relative   md:w-8 md:h-12">
+            <Image
+              src="/carticon.png"
+              alt=""
+              fill
+              sizes="100%"
+              className="object-contain"
+            />
+          </div> : <Button colorScheme="red">Add product</Button>
+        }
+
       </div>
     </Link>
   );
